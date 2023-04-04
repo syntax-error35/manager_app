9239cfdd27c09b3608f02e9f1c9678afb6d81232
Rushnan Muntaka <rushnan.singularity@gmail.com>
2023-04-04 15:40:58 +0600

    [added] rest of the files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:manager_app/screens/login/login_screen.dart';
+import 'package:manager_app/screens/user_list/user_list_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async{
   runApp(const ManagerApp());
 }
 
@@ -11,13 +14,13 @@ class ManagerApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.teal,
       ),
-      home: const LoginScreen(),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
